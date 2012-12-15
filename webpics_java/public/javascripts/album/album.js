2a3fcbce03404335d11cad1eab817e829b994d74
sveri <github@sveri.de>
2012-12-15 21:43:12 +0100

    rotate works now album js now in own file

--- webpics_java/public/javascripts/album/album.js
+++ webpics_java/public/javascripts/album/album.js
@@ -1,11 +1,50 @@
 $(document).ready(function() {	
-	Galleria.loadTheme('@routes.Assets.at("javascripts/galleria/themes/classic/galleria.classic.min.js")');
-	Galleria.run('#galleria');
-	var gallery = Galleria.get(0);
+
+	var value = 0;
 	
-	
-	$('#fullscreen').click(function() {
-        gallery.enterFullscreen(); // will go full screen when the #fullscreen element is clicked
-    });
+	Galleria.loadTheme('/assets/javascripts/galleria/themes/classic/galleria.classic.min.js');
+	    var rotVal = 0;
+		
+		Galleria.ready(function() {
+		    var gallery = this; 
+			
+	        gallery.attachKeyboard({
+	            left: gallery.prev,
+	            right: gallery.next,
+	            up: function(){
+    				rotVal += 90;
+	            	$('#galleria').find('.galleria-stage').find('.galleria-images').find('.galleria-image').each(function(){
+	    				$(this).find('img').rotate(rotVal);
+	    			});
+	            },
+	        	down: function(){
+    				rotVal -= 90;
+	            	$('#galleria').find('.galleria-stage').find('.galleria-images').find('.galleria-image').each(function(){
+	    				$(this).find('img').rotate(rotVal);
+    				});
+	        	}
+
+	        });
+	     });
+		
+		Galleria.run('#galleria');
+		var gallery = Galleria.get(0);
+
+		$('#rotate-right').click(function() {
+			rotVal += 90;
+			$('#galleria').find('.galleria-stage').find('.galleria-images').find('.galleria-image').each(function(){
+				$(this).find('img').rotate(rotVal);
+			});
+		});
+		$('#rotate-left').click(function() {
+			rotVal -= 90;
+			$('#galleria').find('.galleria-stage').find('.galleria-images').find('.galleria-image').each(function(){
+				$(this).find('img').rotate(rotVal);
+			});
+		});
+		
+		$('#fullscreen').click(function() {
+	        gallery.enterFullscreen(); // will go full screen when the #fullscreen element is clicked
+	    });
 	
 });
