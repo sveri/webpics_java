93880aab288a2a0d55f4165c2b6de86b202bce91
sveri <github@sveri.de>
2012-12-18 20:46:35 +0100

    corrected inital password

--- webpics_java/conf/evolutions/default/2.sql
+++ webpics_java/conf/evolutions/default/2.sql
@@ -1,16 +1,16 @@
 # --- !Ups
 
-INSERT INTO `security_role` (`id`, `role_name`) VALUES
-(1, 'admin'),
-(2, 'user'),
-(2, 'viewer');
-
 
 INSERT INTO `users` (`id`, `email`, `name`, `last_login`, `active`, `email_validated`) VALUES
 (1, 'admin@example.com', 'admin', '2012-10-07 15:36:39', 1, 1);
 
+INSERT INTO `security_role` (`id`, `role_name`) VALUES
+(1, 'admin'),
+(2, 'user'),
+(3, 'viewer');
+
 INSERT INTO `linked_account` (`id`, `user_id`, `provider_user_id`, `provider_key`) VALUES
-(1, 1, '$10$2xfCbZbTs0lWuTYEfr.Sle2kQxfSnYOanzdpU3cj8Nx2KOqw5eqrS', 'password');
+(1, 1, '$2a$10$F78Dy7OdmJb.ajUmEUwMpemnLbhjURMybLsJlUfRBepn8mzzzdbsK', 'password');
 
 
 INSERT INTO `users_security_role` (`users_id`, `security_role_id`) VALUES
@@ -22,3 +22,9 @@ INSERT INTO `album` (`id`, `name`) VALUES
 
 
 # --- !Downs
+
+TRUNCATE TABLE security_role;
+TRUNCATE TABLE users;
+TRUNCATE TABLE linked_account;
+TRUNCATE TABLE users_security_role;
+TRUNCATE TABLE album;
